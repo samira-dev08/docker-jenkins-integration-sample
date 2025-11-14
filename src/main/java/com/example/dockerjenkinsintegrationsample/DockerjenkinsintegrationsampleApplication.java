1e637eaa8bccecd64555bb770eb9fa57084df31d
Samira08 <samireatluhanova@gmail.com>
2025-11-14 13:19:01 +0100

    first commit

---
+++ src/main/java/com/example/dockerjenkinsintegrationsample/DockerjenkinsintegrationsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsintegrationsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsintegrationsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkinsintegrationsample/DockerjenkinsintegrationsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsintegrationsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
